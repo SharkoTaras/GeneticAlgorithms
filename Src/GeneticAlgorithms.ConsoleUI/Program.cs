cabbb0a48644353ff436b99e94138c22146d1bf2
SamaelQW <49348304+SamaelQW@users.noreply.github.com>
2021-02-25 19:57:07 +0200

    startin multi dimention processing

--- Src/GeneticAlgorithms.ConsoleUI/Program.cs
+++ Src/GeneticAlgorithms.ConsoleUI/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using GeneticAlgorithms.Core.Entities;
 using GeneticAlgorithms.Core.Extensions;
 using GeneticAlgorithms.Core.Generators;
@@ -8,9 +10,11 @@ namespace GeneticAlgorithms.ConsoleUI
 {
     internal class Program
     {
+        //public static Func<double, double> f = (x) => x * x - 1;
         public static Func<double, double> f = (x) => (x * Math.Sin(10 * Math.PI * x)) + 1;
 
         public static Func<BitString, double> M = (x) => ((double)3 / ((1 << Bits) - 1) * x.ToNumber()) - 1;
+        //public static Func<BitString, double> M = (x) => ((double)3 / ((1 << Bits) - 1) * x.ToNumber()) - 1;
 
         public static Func<BitString, double> eval = str => f(M(str));
 
@@ -29,17 +33,37 @@ namespace GeneticAlgorithms.ConsoleUI
         private static void Main(string[] args)
         {
             Init();
+            Console.Write("Attempts: ");
+            var attempts = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"Print every attempt? (y/n): ");
+            var input = Console.ReadLine();
+            var print = input.Equals("Y", StringComparison.InvariantCultureIgnoreCase);
+            var tries = new List<BitString>();
             var algorithmContext = new AlgorithmContext(N, Nt, Pm, Pc, Maxt, H);
-            var p0 = new PopulationGenerator().Generate(N, (uint)Bits);
+            for (var i = 0; i < attempts; i++)
+            {
+                if (print)
+                {
+                    Console.WriteLine(new string('-', 20));
+                }
+                var p0 = new PopulationGenerator().Generate(N, (uint)Bits);
 
-            var minimazer = new Minimazer(algorithmContext, p0, f, M);
-            var bitstr = minimazer.Minimize();
-
-
-            Console.WriteLine($"Bit string: {bitstr}");
-            Console.WriteLine($"Real number: {M(bitstr)}");
-            Console.WriteLine($"f in string: {eval(bitstr)}");
-            Console.Read();
+                var minimazer = new Minimazer(algorithmContext, p0, f, M);
+                var bitstr = minimazer.Minimize();
+                tries.Add(bitstr);
+                if (print)
+                {
+                    Console.WriteLine($"Bit string: {bitstr}");
+                    Console.WriteLine($"Real number: {M(bitstr)}");
+                    Console.WriteLine($"f in string: {eval(bitstr)}");
+                    Console.WriteLine(new string('-', 20));
+                }
+            }
+            Console.WriteLine("Best result:");
+            var str = tries.OrderBy(s => eval(s)).First();
+            Console.WriteLine($"Bit string: {str}");
+            Console.WriteLine($"Real number: {M(str)}");
+            Console.WriteLine($"f in string: {eval(str)}");
         }
 
         private static void Init()
---
+++ Src/GeneticAlgorithms.Core/MultiDimention/MDBitString.cs
@@ -0,0 +1,21 @@
+﻿using GeneticAlgorithms.Core.Entities;
+
+namespace GeneticAlgorithms.Core.MultiDimention
+{
+    public class MDBitString : BitString
+    {
+        #region Constructors
+        public MDBitString()
+        {
+        }
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Overrides
+        public override string ToString() => base.ToString();
+        #endregion
+    }
+}
